ffcdf5c5490d19011ac75db61c6305a2bedce8d1
tekintr <ismailtekin63@gmail.com>
2017-02-13 09:18:46 +0300

    Delete RoninKarma & Skarner & Matrix - Added TekinKarma

--- Karma/TekinKarma/Properties/AssemblyInfo.cs
+++ Karma/TekinKarma/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MatrixLucian")]
+[assembly: AssemblyTitle("TekinKarma")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MatrixLucian")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TekinKarma")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3203502a-4253-4edd-9331-74a49c616ed2")]
+[assembly: Guid("ae0dcb74-af63-4662-a404-cd81722cd440")]
 
 // Version information for an assembly consists of the following four values:
 //
